aa2998679b02a811e9937f803d50e2e5e287500d
Yauhen <silent_control@tut.by>
2020-12-19 15:17:43 +0300

    Update Header link (path id). Update detail-page to use data

--- src/pages/detail-page/detail-page.jsx
+++ src/pages/detail-page/detail-page.jsx
@@ -1,5 +1,33 @@
-import styles from './detail-page.module.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-export const DetailPage = () => (
-  <h1 className={styles.heading}>Detail Page</h1>
-);
+import { Details } from '../../components/details';
+import { Episodes } from '../../components/episodes';
+
+import { requestApiDetails, requestApiEpisodes } from '../../actions';
+
+export const selectDetails = store => store?.data?.details;
+export const selectEpisodes = store => store?.data?.episodes;
+
+export const DetailPage = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const details = useSelector(selectDetails);
+  const episodes = useSelector(selectEpisodes);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(requestApiDetails(id));
+      dispatch(requestApiEpisodes(id));
+    }
+  }, [id]);
+
+  return (
+    <section>
+      <Details details={details} />
+      <Episodes episodes={episodes} />
+    </section>
+  );
+};
